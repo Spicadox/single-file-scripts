0e0e6dd531d876ba4f6ea972467706e24cc762ad
Sam Phung <sphung@uncc.edu>
2022-04-30 19:37:09 -0400

    Clean up

--- replace_image_url_script.js
+++ replace_image_url_script.js
@@ -42,27 +42,3 @@
 		return;
     });
 })();
-
-
-// (() => {
-// dispatchEvent(new CustomEvent("single-file-user-script-init"));
-//
-// addEventListener("single-file-on-before-capture-request", () => {
-//     link_holder = document.getElementById("links-holder");
-//     link_holder.remove();
-//     // Get the image element that has a src attribute and an empty alt(use to ignore profile images)
-//     const imgElementsSelector = "img[src][alt='']:not([src=''])";
-//
-//     // Regex grouping[0] is the original string
-//     // Group[1] is the entire string before =s where everything after the domain name is between 30-85 characters
-//     // Group[2] will be used and set to 0 to obtain the max resolution of the image
-//     const pattern = "(https:\/\/yt3\.ggpht\.com\/.{30,85}=[a-z]{1})(.*)";
-//     Array.from(content.images).forEach(image => {
-//         try{
-//             group_match = image.src.match(pattern);
-//             new_img_src = group_match[1] + '0';
-//             image.src = new_img_src;
-//         }catch(error) {
-//             console.log(error);
-//         }
-// })});})();
